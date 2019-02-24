6b133ff8f62d9ac7b680a6895ce6beeec5565ce5
Nick <nhorton04@gmail.com>
2019-02-23 17:01:57 -0800

    set up user model

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
