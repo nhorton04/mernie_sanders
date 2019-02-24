6b180d426a221dd056d910ef2e3e6f5935956d7a
Nick <nhorton04@gmail.com>
2019-02-23 17:17:01 -0800

    add register route to users profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-// @route  GET api/users/test
-// @desc   Tests users route
-// @access Public
+const User = require("../../models/User");
+
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 module.exports = router;
