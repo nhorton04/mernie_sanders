ec0502d02b244bf6c1051aa92163f1aa49a6a72e
Nick <nhorton04@gmail.com>
2019-03-01 00:37:44 -0800

    add login photo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
@@ -8,6 +10,14 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
+import "./App.css";
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
