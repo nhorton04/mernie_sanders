02806badbd9b797e1cd0e9066e818faa84328fa9
Nick <nhorton04@gmail.com>
2019-03-05 21:45:01 -0800

    delete experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -59,6 +59,22 @@ export const addEducation = (eduData, history) => dispatch => {
       })
     );
 };
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {
