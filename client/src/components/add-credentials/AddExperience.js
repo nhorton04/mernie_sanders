e530bb788135a10acde6d72887795cd9ff0bd9f9
Nick <nhorton04@gmail.com>
2019-03-05 19:02:00 -0800

    add experience

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { TextFieldGroup } from "../common/TextFieldGroup";
+import { TextFieldAreaGroup } from "../common/TextFieldAreaGroup";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class AddExperience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "",
+      title: "",
+      location: "",
+      from: "",
+      to: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default AddExperience;
