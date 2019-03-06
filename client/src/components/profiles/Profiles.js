8ae67f36de230f31f7f147515d6dbaa12bd7c9d4
Nick <nhorton04@gmail.com>
2019-03-06 15:29:54 -0800

    view all profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../common/Spinner";
 import { getProfiles } from "../../actions/profileActions";
+import ProfileItem from "./ProfileItem";
 
 class Profiles extends Component {
   componentDidMount() {
@@ -16,6 +17,9 @@ class Profiles extends Component {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
         profileItems = <h4>No profiles found...</h4>;
       }
