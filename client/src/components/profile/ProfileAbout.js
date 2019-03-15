54f102b61e87e721c3dbfc9bd619827133ce4cbe
Nick <nhorton04@gmail.com>
2019-03-15 00:10:31 -0700

    add security noopener noreferrer

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -7,33 +7,29 @@ class ProfileAbout extends Component {
     const { profile } = this.props;
 
     const firstName = profile.user.name.trim().split(" ")[0];
+
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">{firstName}s Bio</h3>
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
             <p className="lead">
-              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} doesn't have a bio : (</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
             </p>
             <hr />
             <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
               <div className="d-flex flex-wrap justify-content-center align-items-center">
-                <div className="p-3">
-                  <i className="fa fa-check" /> HTML
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> CSS
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> JavaScript
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> Python
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> C#
-                </div>
+                {skills}
               </div>
             </div>
           </div>
@@ -42,5 +38,9 @@ class ProfileAbout extends Component {
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
